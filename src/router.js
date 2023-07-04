6eb25f7a8701a6aee55aebd58c78defc98639fd4
niusz <163yourba@163.com>
2023-07-04 15:05:46 +0800

    homeSearch

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 const test = require('./controller/test')
+const home_search = require('./controller/home_search')
 
 module.exports = (app) => {
     app.use('/api/test', test)
+    app.use('/api/home_search', home_search)
 }
